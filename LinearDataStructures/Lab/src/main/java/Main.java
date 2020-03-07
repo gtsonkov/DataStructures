5993334e61ae25ac7096a18ca25984721f504266
Georgi Tsonkov <gtsonkov@abv.bg>
2020-03-07 17:14:43 +0100

    -init SinglyLinkedList -all

--- LinearDataStructures/Lab/src/main/java/Main.java
+++ LinearDataStructures/Lab/src/main/java/Main.java
@@ -1,5 +1,13 @@
+import implementations.SinglyLinkedList;
+
 public class Main {
     public static void main(String[] args) {
-
+        SinglyLinkedList<String> myList = new SinglyLinkedList<>();
+        for (int i = 0; i <20 ; i++) {
+            myList.addLast(""+i);
+        }
+        for (String part :myList){
+            System.out.println(part);
+        }
     }
 }
--- LinearDataStructures/Lab/src/main/java/implementations/SinglyLinkedList.java
+++ LinearDataStructures/Lab/src/main/java/implementations/SinglyLinkedList.java
@@ -2,52 +2,137 @@ package implementations;
 
 import interfaces.LinkedList;
 
+import javax.swing.*;
 import java.util.Iterator;
 
 public class SinglyLinkedList<E> implements LinkedList<E> {
 
+    private Node<E> top;
+    private int size;
+
+    private static class Node<E>{
+
+        public E value;
+        private Node<E> nexElement;
+       public Node(E element){
+            this.value = element;
+        }
+    }
+    public SinglyLinkedList()
+    {
+        this.top = null;
+        this.size = 0;
+    }
+
     @Override
     public void addFirst(E element) {
+        Node<E> tmp = new Node<>(element);
 
+        tmp.nexElement = this.top;
+        this.top = tmp;
+        this.size++;
     }
 
     @Override
     public void addLast(E element) {
+        Node<E> newElement = new Node<>(element);
+        if (this.isEmpty()) {
+            this.top = newElement;
+        } else {
 
+            Node<E> current = this.top;
+
+            while (current.nexElement != null) {
+                current = current.nexElement;
+            }
+
+            current.nexElement = newElement;
+        }
+        this.size++;
     }
 
     @Override
     public E removeFirst() {
-        return null;
+        ensureNonEmpty();
+        E element = this.top.value;
+
+        this.top = this.top.nexElement;
+
+        this.size--;
+        return element;
     }
 
     @Override
     public E removeLast() {
-        return null;
+        ensureNonEmpty();
+        E value = null;
+        if (this.size == 1){
+            value = this.top.value;
+            this.top = null;
+        } else {
+            Node<E> PreLast = this.top;
+            Node<E> last = this.top.nexElement;
+
+            while (last.nexElement != null){
+                PreLast = last;
+                last= PreLast.nexElement;
+            }
+            value = last.value;
+            PreLast.nexElement = null;
+        }
+
+        this.size--;
+        return value;
     }
 
     @Override
     public E getFirst() {
-        return null;
+        ensureNonEmpty();
+        return this.top.value;
     }
 
     @Override
     public E getLast() {
-        return null;
+        ensureNonEmpty();
+        Node<E> current = this.top;
+
+        while (current.nexElement != null){
+            current = current.nexElement;
+        }
+        return current.value;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return (this.size == 0);
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            private Node<E> _top = top;
+            @Override
+            public boolean hasNext() {
+                return (_top != null);
+            }
+
+            @Override
+            public E next() {
+                E value = _top.value;
+                _top = _top.nexElement;
+                return value;
+            }
+        };
+    }
+
+    private void ensureNonEmpty(){
+        if (isEmpty()){
+            throw new IllegalStateException("No elements!");
+        }
     }
 }
