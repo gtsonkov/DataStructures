e36168b6763504b3ef68cab4f5630d2666dd6b4e
Georgi Tsonkov <gtsonkov@abv.bg>
2020-03-07 14:21:16 +0100

    init Stack -all

--- LinearDataStructures/Lab/src/main/java/implementations/Stack.java
+++ LinearDataStructures/Lab/src/main/java/implementations/Stack.java
@@ -5,34 +5,81 @@ import interfaces.AbstractStack;
 import java.util.Iterator;
 
 public class Stack<E> implements AbstractStack<E> {
+    private Node<E> top;
+    private int size;
+
+    private static class Node<E>{
+
+    private E value;
+    private Node<E> nexElement;
+    Node(E element){
+        this.value = element;
+     }
+    }
+
+    public Stack(){
+        this.top = null;
+        this.size = 0;
+    }
 
     @Override
     public void push(E element) {
+        Node<E> newElement = new Node<>(element);
 
+        newElement.nexElement = this.top;
+        this.top = newElement;
+
+        this.size++;
     }
 
     @Override
     public E pop() {
-        return null;
+        ensureNonEmpty();
+
+        Node<E> tmp = this.top;
+        this.top = tmp.nexElement;
+        this.size--;
+        return  tmp.value;
     }
 
     @Override
     public E peek() {
-        return null;
+        ensureNonEmpty();
+
+        return this.top.value;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.size == 0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            private Node<E> current = top;
+            @Override
+            public boolean hasNext() {
+                return this.current.nexElement != null;
+            }
+
+            @Override
+            public E next() {
+                E value = this.current.value;
+                this.current = this.current.nexElement;
+                return value;
+            }
+        };
+    }
+
+    private void ensureNonEmpty(){
+        if (this.isEmpty()){
+            throw new IllegalStateException("No elements in this stack");
+        }
     }
 }
