8740a261ec515bea664c774bcda134bf4a942c78
Georgi Tsonkov <gtsonkov@abv.bg>
2020-03-12 20:10:54 +0100

    init -Exercise (LinearDataStructures)

---
+++ LinearDataStructures/Exercise/src/main/java/implementations/Queue.java
@@ -0,0 +1,93 @@
+package implementations;
+
+import interfaces.AbstractQueue;
+
+import java.util.Iterator;
+
+public class Queue<E> implements AbstractQueue<E> {
+    private Node<E> top;
+    private Node<E> last;
+    private int size;
+
+    private static class Node<E> {
+        private E element;
+        private Node<E> next;
+        private Node(E element) {
+            this.element = element;
+        }
+    }
+
+    public Queue() {
+    }
+
+    @Override
+    public void offer(E element) {
+        Node<E> newNode = new Node<>(element);
+        if (this.top == null) {
+            this.top = newNode;
+        } else {
+            Node<E> current = this.top;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        this.size++;
+    }
+
+    @Override
+    public E poll() {
+        ensureNonEmpty();
+        E element = this.top.element;
+        if (this.size == 1) {
+            this.top = null;
+        } else {
+            Node<E> next = this.top.next;
+            this.top.next = null;
+            this.top = next;
+        }
+        this.size--;
+        return element;
+    }
+
+    @Override
+    public E peek() {
+        ensureNonEmpty();
+        return this.top.element;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private Node<E> current = top;
+
+            @Override
+            public boolean hasNext() {
+                return this.current != null;
+            }
+
+            @Override
+            public E next() {
+                E element = this.current.element;
+                this.current = this.current.next;
+                return element;
+            }
+        };
+    }
+
+    private void ensureNonEmpty() {
+        if (this.size == 0) {
+            throw new IllegalStateException("Illegal operation on empty stack");
+        }
+    }
+}
