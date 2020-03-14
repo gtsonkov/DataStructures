8d2aa310c8f3569ecb9b06f1ecff4400bae79ca8
Georgi Tsonkov <gtsonkov@abv.bg>
2020-03-14 19:16:38 +0100

    Exercise -init ReversedList class and interface

--- LinearDataStructures/Exercise/src/main/java/implementations/ReversedList.java
+++ LinearDataStructures/Exercise/src/main/java/implementations/ReversedList.java
@@ -1,4 +1,35 @@
 package implementations;
 
-public class ReversedList {
+import java.util.Iterator;
+
+public class ReversedList<E> implements interfaces.ReversedList<E> {
+    @Override
+    public void Add(E element) {
+
+    }
+
+    @Override
+    public int Size() {
+        return 0;
+    }
+
+    @Override
+    public int Capacity() {
+        return 0;
+    }
+
+    @Override
+    public E Get(E index) {
+        return null;
+    }
+
+    @Override
+    public void RemoveAt(int index) {
+
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
 }
--- LinearDataStructures/Exercise/src/main/java/interfaces/ReversedList.java
+++ LinearDataStructures/Exercise/src/main/java/interfaces/ReversedList.java
@@ -1,4 +1,9 @@
 package interfaces;
 
-public interface ReversedList {
+public interface ReversedList<E> extends Iterable<E> {
+  void Add(E element);
+  int Size();
+  int Capacity();
+  E Get(E index);
+  void RemoveAt(int index);
 }
