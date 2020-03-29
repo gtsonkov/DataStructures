d8c11ce984e6d38a70de0b04b9290e3f428079f0
Georgi Tsonkov <gtsonkov@abv.bg>
2020-03-29 12:31:20 +0200

    asIndentedPreOrder

--- HeapsAndBST/Lab/src/main/java/implementations/BinaryTree.java
+++ HeapsAndBST/Lab/src/main/java/implementations/BinaryTree.java
@@ -19,27 +19,48 @@ public class BinaryTree<E> implements AbstractBinaryTree<E> {
 
     @Override
     public E getKey() {
-        return null;
+        return this.key;
     }
 
     @Override
     public AbstractBinaryTree<E> getLeft() {
-        return null;
+        return this.leftChild;
     }
 
     @Override
     public AbstractBinaryTree<E> getRight() {
-        return null;
+        return this.rightChild;
     }
 
     @Override
     public void setKey(E key) {
-
+        this.key = key;
     }
 
     @Override
     public String asIndentedPreOrder(int indent) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        String padding = createPadding(indent) + this.key;
+        sb.append(padding);
+
+        if (this.getLeft() != null ){
+          String curr =  this.getLeft().asIndentedPreOrder(indent+2);
+          sb.append(System.lineSeparator()).append(curr);
+        }
+
+        if (this.getRight() != null ){
+            String curr = this.getRight().asIndentedPreOrder(indent+2);
+            sb.append(System.lineSeparator()).append(curr);
+        }
+        return sb.toString();
+    }
+
+    private String createPadding(int indent) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < indent ; i++) {
+            sb.append(" ");
+        }
+        return  sb.toString();
     }
 
     @Override
