8397178f1b1ec925ecf696bab533c034ea1b252f
Georgi Tsonkov <gtsonkov@abv.bg>
2020-03-30 20:40:14 +0200

    init MaxHeap

--- HeapsAndBST/Lab/src/main/java/implementations/MaxHeap.java
+++ HeapsAndBST/Lab/src/main/java/implementations/MaxHeap.java
@@ -7,19 +7,46 @@ import java.util.Collections;
 import java.util.List;
 
 public class MaxHeap<E extends Comparable<E>> implements Heap<E> {
+    private List<E> elements;
+
+    public MaxHeap(){
+        this.elements = new ArrayList<>();
+    }
 
     @Override
     public int size() {
-        return 0;
+        return this.elements.size();
     }
 
     @Override
     public void add(E element) {
+        this.elements.add(element);
+        heaapifyUp(this.size() -1);
+    }
 
+    private void heaapifyUp(int index) {
+        int parentIndex = getParentIndex(index);
+        while (index > 0 && isLess(index,parentIndex)){
+            Collections.swap(this.elements,index,parentIndex);
+            index = parentIndex;
+        }
+    }
+
+    private boolean isLess(int childIndex, int parentIndex) {
+
+        return  getAt(childIndex).compareTo(getAt(parentIndex)) > 0;
+    }
+
+    private E getAt(int index){
+        return this.elements.get(index);
+    }
+
+    private int getParentIndex(int index) {
+        return ((index-1)/2);
     }
 
     @Override
     public E peek() {
-        return null;
+        return this.elements.get(0);
     }
 }
